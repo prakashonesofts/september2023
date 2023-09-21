13a6383af109f479f616b880ae4ee011b72cab5c
prakashrkps <prakashrkps@gmail.com>
2023-09-21 14:21:01 +0530

    added controller

---
+++ src/main/java/in/onesoft/SampleGit/SampleController.java
@@ -0,0 +1,13 @@
+package in.onesoft.SampleGit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping(value = "/getdetails")
+    public String get(){
+        return "Helloworld";
+    }
+}
